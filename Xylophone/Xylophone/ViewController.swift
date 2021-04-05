261b5cbf894ba12e895307c8d1b2d52c6ef692a2
Kirti Parghi <kirtiparghi@gmail.com>
2021-04-05 14:09:17 -0400

    logic added.

--- Xylophone/Xylophone/ViewController.swift
+++ Xylophone/Xylophone/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        sender.layer.opacity = 0.5
+        playSound(soundName: sender.currentTitle!)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.layer.opacity = 1
+        }
+    }
+    
+    func playSound(soundName: String) {
+        let path = Bundle.main.path(forResource: soundName, ofType: "wav")
+        let url = URL(fileURLWithPath: path!)
 
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            player = try AVAudioPlayer(contentsOf: url)
+            player!.volume = 1.0
+            player!.prepareToPlay()
+            player!.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
 
 }
 
